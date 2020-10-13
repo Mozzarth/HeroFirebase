e024f9a50be6f3f14baae6028936160fc5863efa
Mozzarth <moisescaicedo15@gmail.com>
2020-10-13 08:08:08 -0500

    ADD Router and componencts hero/s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
